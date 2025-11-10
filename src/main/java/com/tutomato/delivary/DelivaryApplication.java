131d1c39383c7ab5c355a177b95da4a08100f0ef
jongwan <jongbell4@gmail.com>
2025-11-10 22:13:08 +0900

    first init

---
+++ src/main/java/com/tutomato/delivary/DelivaryApplication.java
@@ -0,0 +1,13 @@
+package com.tutomato.delivary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelivaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelivaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tutomato/delivary/DelivaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutomato.delivary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelivaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
