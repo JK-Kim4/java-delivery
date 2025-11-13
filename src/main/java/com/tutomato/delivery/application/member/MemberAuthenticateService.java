8053ee086314f220869b10104180ad2ccfa8a810
jongwan <jongbell4@gmail.com>
2025-11-13 22:56:45 +0900

    feat: member identifier 생성 로직 추가

--- src/main/java/com/tutomato/delivery/application/member/MemberAuthenticateService.java
+++ src/main/java/com/tutomato/delivery/application/member/MemberAuthenticateService.java
@@ -43,17 +43,25 @@ public class MemberAuthenticateService {
         // 2. password 일치 여부 검증
         validationPassword(member, command.rawPassword());
 
-        // 3.토큰 생성
-        // TODO: Token에 포함되어있는 사용자 인증갑 정의 필요
-        Tokens tokens = jwtTokenProvider.generateTokenPair(member.getAccount(), now);
+        // 3. identifier 생성
+        String identifier = generateMemberIdentifier(member);
 
+        // 4.토큰 생성
+        Tokens tokens = jwtTokenProvider.generateTokenPair(identifier, now);
+
+        // 5. 반환
         return MemberAuthenticateResult.of(member, tokens);
     }
-
 
     private void validationPassword(Member member, String rawPassword) {
         if (!member.isCorrectPassword(rawPassword, CryptoCipherFactory.get(CryptoAlgorithm.SHA256))) {
             throw new InvalidPasswordException("사용자의 비밀번호가 일치하지않습니다.");
         }
+    }
+
+    // TODO: Token에 포함되어있는 사용자 인증값 정의 필요
+    // 현재 access token 내 사용자 인증 정보는 {memberId},{memberAccount} 정보를 문자열 형태로 전달
+    private String generateMemberIdentifier(Member member) {
+        return member.getId() + "," + member.getAccount();
     }
 }
