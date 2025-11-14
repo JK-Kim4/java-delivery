da0f13d02c03e6ee2a0770c357b567c4a1b276dc
jongwan <jongbell4@gmail.com>
2025-11-15 07:52:11 +0900

    env: 환경 변수 수정 - Swagger authorization 설정 추가 - Request Header Resolver 등록

--- src/main/java/com/tutomato/delivery/common/resolver/FromAuthHeaderArgumentResolver.java
+++ src/main/java/com/tutomato/delivery/common/resolver/FromAuthHeaderArgumentResolver.java
@@ -4,11 +4,13 @@ import com.tutomato.delivery.common.annotation.FromAuthHeader;
 import com.tutomato.delivery.common.extractor.AuthMemberExtractor;
 import com.tutomato.delivery.common.extractor.AuthMemberRequest;
 import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Component
 public class FromAuthHeaderArgumentResolver implements HandlerMethodArgumentResolver {
 
     private final AuthMemberExtractor authMemberExtractor;
